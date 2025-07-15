cd8f87d7ecfc54dad4de28fd73c87aa1cf03af59
Oksana <oksanavasilevna77@gmail.com>
2025-07-15 22:21:15 +0300

    fix: fix: unexpected behavior

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,18 +7,28 @@ import { useState } from "react";
 import { generatePdf } from "../helpers/pdf";
 import { useSelector } from "react-redux";
 import { selectOrders } from "../store/orders/selectors";
+import { useAppDispatch } from "../hooks/useDispatch";
+import { setCurrentOrder } from "../store/orders/slice";
 
 const HomePage = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useAppDispatch();
   const handleClose = () => {
     setIsOpen(false);
   };
   const orders = useSelector(selectOrders);
+
   return (
     <section className="home-page">
       <Container>
         <div className="wrapper-btn">
-          <Button onClick={() => setIsOpen(true)} variant="contained">
+          <Button
+            onClick={() => {
+              dispatch(setCurrentOrder(null));
+              setIsOpen(true);
+            }}
+            variant="contained"
+          >
             Нове замовлення
           </Button>
           <Button onClick={() => generatePdf(orders)} variant="contained">
